d25d0318af3d9cdebbddeff550e061cb57e88bad
StivenSaldarriagaMayorga <ssaldarriagam@unal.edu.co>
2024-03-05 04:10:03 -0500

    Primero

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,12 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre= None, salon= None):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def salon(self):
+        return self._salon
+    def nombre(self):
+        return self._nombre
+    def __str__(self):
+        return self._nombre + " remoto"
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,56 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 6"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
-        self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+    def __init__(self, grupo=None, asignaturas=None, estudiantes=None):
+        if grupo is None:
+            self._grupo = "grupo predeterminado"
+        else:    
+            self._grupo = grupo
+        if asignaturas is None:
+            self._asignaturas = []
+        else:
+            self.asignaturas = asignaturas
+        if estudiantes is None:
+            self.listadoAlumnos = []
+        else:
+            self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    
+    
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista=[]
+            lista.append(alumno)
+            self.listadoAlumnos = self.listadoAlumnos + lista
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return "Grupo de estudiante: " + self._grupo
+
+
+
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    def asignarNombre(cls, nombre=None):
+            if nombre is None:
+                cls.grado = "Grado 6"
+            else:
+                cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+
+    # @ classmethod
+    # def asignarNombre(cls, nombre="Grado 6"):
+    #         cls.grado = nombre
+        
+    # @ classmethod
+    # def asignarNombre(cls, nombre="Grado 4"):
+    #         cls.grado = nombre
